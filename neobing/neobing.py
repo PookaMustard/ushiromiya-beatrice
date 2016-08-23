2756fbbafc317f7ac9750725ea0478c29eb4c355
Certainty Witch <pooka251@gmail.com>
2016-08-23 23:42:58 +0200

    Update neobing.py

--- neobing/neobing.py
+++ neobing/neobing.py
@@ -17,45 +17,45 @@ class bing:
     Uses the Python module py_bing_search as a frontend for Red"""
     
     def __init__(self, bot):
-    	self.bot = bot
-	self.PREFIXES = bot_settings.prefixes 
+        self.bot = bot
+		self.PREFIXES = bot_settings.prefixes 
 		
-    def getfrombing(self, apikey, text, limit, operation):
-	if operation == 'moderateimagesearch':
-		bing_obj = PyBingImageSearch(api_key, text, custom_params="&Adult='Moderate'")
-	result= bing_obj.search(limit=limit, format='json')
-	return result
+	def getfrombing(self, apikey, text, limit, operation):
+		if operation == 'moderateimagesearch':
+			bing_obj = PyBingImageSearch(api_key, text, custom_params="&Adult='Moderate'")
+		result= bing_obj.search(limit=limit, format='json')
+		return result
 		
-    def obtainresult(result, operation):
-	if operation == 'moderateimagesearch':
-		maxnum = len(result)
-		return result[randint(1, maxnum)].mediaurl
+	def obtainresult(result, operation):
+		if operation == 'moderateimagesearch':
+			maxnum = len(result)
+			return result[randint(1, maxnum)].mediaurl
 			
     @commands.command(pass_context=True)
     @checks.admin_or_permissions(manage_server=True)
     async def apikey_neobing(self, ctx, key):
         """Set the Bing API key."""
-	settings = loadauth()
-	settings['apikey'] = key
-	saveauth(settings)
-	return await self.bot.say("Bing API key saved.")
+		settings = loadauth()
+		settings['apikey'] = key
+		saveauth(settings)
+		return await self.bot.say("Bing API key saved.")
 		
     @commands.command()
     async def neobing(self, *, text):
-	"""Searches Bing for images."""
-	settings = loadauth()
-	operation = 'moderateimagesearch'
-	if settings['apikey'] == '' or settings['apikey'] == 'blank':
-		return await self.bot.say("` This cog wasn't configured properly. If you're the owner, add your API key.`")
-	apikey = settings['apikey']
-	if text.split(' ', 1)[0].lower() == 'random':
-		text = text.replace('random ', '', 1)
-		limit = 100
-	else:
-		limit = 1
-	result = getfrombing(apikey, text, limit, operation)
-	bottext = obtainresult(result, operation)
-	return await self.bot.say(bottext)
+		"""Searches Bing for images."""
+		settings = loadauth()
+		operation = 'moderateimagesearch'
+		if settings['apikey'] == '' or settings['apikey'] == 'blank':
+			return await self.bot.say("` This cog wasn't configured properly. If you're the owner, add your API key.`")
+		apikey = settings['apikey']
+		if text.split(' ', 1)[0].lower() == 'random':
+			text = text.replace('random ', '', 1)
+			limit = 100
+		else:
+			limit = 1
+		result = getfrombing(apikey, text, limit, operation)
+		bottext = obtainresult(result, operation)
+		return await self.bot.say(bottext)
 
 
 def saveauth(settings):
@@ -73,7 +73,7 @@ def loadauth():
 def check_folders():
     if not os.path.exists(DATADIR):
         print("Creating data directory for Command Request cog")
-	os.mkdir(DATADIR)
+        os.mkdir(DATADIR)
             
 def check_files():
     if not fileIO(SETTINGS, "check"):
