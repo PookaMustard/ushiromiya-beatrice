37d516169c446e694082913e5a79386031b8cf46
PookaMustard <pooka251@gmail.com>
2016-08-24 23:27:35 +0200

    Removing unused import, updating styling

--- commandrequest/commandrequest.py
+++ commandrequest/commandrequest.py
@@ -1,9 +1,9 @@
-import discord
 from discord.ext import commands
 from cogs.utils import checks
 from .utils.dataIO import fileIO
 import json
 import os
+
 
 class CommandRequest:
 
@@ -16,30 +16,37 @@ class CommandRequest:
         A modified version of the debug command, with help from Calebj."""
         channelid = loadauth()
         if channelid == '':
-            return await self.bot.say("You haven't set the channel ID for command requests correctly. " +
-                "Use `[p]savechannelid [your channel ID here]` to set the command request channel. Use " +
-                "Discord's Developer Mode to copy the ID of the channel you wish to set for receiving " +
-                "command requests.")
+            return await self.bot.say("You haven't set the channel ID for " +
+                                      "command requests correctly. Use " +
+                                      "`[p]savechannelid [your channel ID " +
+                                      "here]` to set the command request " +
+                                      "channel. Use Discord's Developer " +
+                                      "Mode to copy the ID of the channel" +
+                                      "you wish to set for receiving " +
+                                      "command requests.")
         author = ctx.message.author
         server = ctx.message.server
-        command = command + " -- Command requested by " + author.name + " from " + "{}".format(server.name)
+        command = command + " -- Command requested by " + \
+            author.name + " from " + "{}".format(server.name)
         command = command.replace("\'", "\\\'")
         channelid = self.bot.get_channel(str(channelid))
         return await self.bot.send_message(channelid, command)
-        
+
     @commands.command()
     @checks.is_owner()
     async def savechannelid(self, channelid):
         """Saves this channel for commandrequests."""
         saveauth(channelid)
         channelidstring = loadauth()
-        return await self.bot.say("Saved. Now using " + channelidstring + " as the channel id for " +
-            "command requests.")
-        
-### GLOBAL JSON FUNCTIONS
+        return await self.bot.say("Saved. Now using " + channelidstring +
+                                  " as the channel id for command " +
+                                  "requests.")
+
+# GLOBAL JSON FUNCTIONS
 
 DATADIR = "data/commandrequests"
 SETTINGS = DATADIR + "/data.json"
+
 
 def saveauth(channelid):
     channelid = channelid
@@ -47,24 +54,28 @@ def saveauth(channelid):
         json.dump(channelid, f)
     return
 
+
 def loadauth():
     channelid = ''
     with open(SETTINGS, 'r') as f:
         channelid = json.load(f)
     return channelid
 
+
 def check_folders():
     if not os.path.exists(DATADIR):
         print("Creating data directory for Command Request cog")
         os.mkdir(DATADIR)
-            
+
+
 def check_files():
     if not fileIO(SETTINGS, "check"):
         channelid = ''
         print("Creating blank data file for Command Request cog")
         fileIO(SETTINGS, "save", channelid)
 
-### BOT SETUP
+# BOT SETUP
+
 
 def setup(bot):
     check_folders()
