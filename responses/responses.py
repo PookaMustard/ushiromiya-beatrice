320653fe91e3cd90d7f91a6e13bfc9f5501a8fd3
Certainty Witch <pooka251@gmail.com>
2016-09-02 16:38:17 +0200

    Chara is coming for you.

--- responses/responses.py
+++ responses/responses.py
@@ -13,7 +13,7 @@ class sao:
         self.timecheckchara = {}
         self.cooldown = 60*3 #60 seconds times the required minutes
 
-    async def check_sao(self, message):
+    async def check(self, message):
         saocheck = ['sao', 'sword art online', 'sword art offline']
         checkmessage=message.content.lower()
         if 'sao' in checkmessage.split() or 'sword art online' in checkmessage or 'sword art offline' in checkmessage and message.author.id != self.bot.user.id:
@@ -22,10 +22,7 @@ class sao:
                 sao = ["SAO? Why don't you read Umineko no Naku Koro ni instead?", "I won't mute you for five minutes for mentioning SAO.",
                     "On another server, you'd be muted for mentioning SAO. Talk about it a lot here!"]
                 await self.bot.send_message(message.channel, randchoice(sao))
-                
-    async def check_chara(self, message):
-        checkmessage=message.content.lower()
-        if 'chara' in checkmessage.split() and message.author.id != self.bot.user.id:
+        elif 'chara' in checkmessage.split() and message.author.id != self.bot.user.id:
             if message.channel.id not in self.timecheckchara or (time.time() - self.timecheckchara[message.channel.id]) > self.cooldown:
                 self.timecheckchara[message.channel.id] = time.time()
                 chance = randint(1,100)
@@ -41,8 +38,7 @@ class sao:
                         return await self.bot.send_message(message.channel, 'https://images-1.discordapp.net/.eJwVx10OwiAMAOC7cABqt_KzXcY0DIGEyUKrL8a7G7-372Nes5vdVNVLdoCjSRrzsKJjcsm2jFF65quJTeMEVuVUz_xUAdxwCX7FiCFuK4VIsNzcf-S8jw4xEEF-t55F76nyZFvaw3x_44IlKg.u4gRjc9h4xSj0qE6LRLG_KtxQDA.gif')
 
 
-
 def setup(bot):
     n = sao(bot)
-    bot.add_listener(n.check_sao, "on_message")
+    bot.add_listener(n.check, "on_message")
     bot.add_cog(n)
